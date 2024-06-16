c2fccc28fb89270a42b44918496fb9c555bd1996
mayank2130 <43805078+Chhavithakur@users.noreply.github.com>
2024-06-16 13:28:27 +0530

    buyer email

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx"
+import { Metadata } from "next"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
@@ -6,10 +7,42 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const formatPrice = (price: number) => {
-  const formatter = new Intl.NumberFormat('en-US', {
+  const formatter = new Intl.NumberFormat('en-IN', {
     style: 'currency',
-    currency: 'USD',
+    currency: 'INR',
   })
 
   return formatter.format(price)
+}
+
+
+export function constructMetadata({
+  title = 'CaseCobra - custom high-quality phone cases',
+  description = 'Create custom high-quality phone cases in seconds',
+  image = '/thumbnail.png',
+  icons = '/favicon.ico',
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [{ url: image }],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [image],
+      creator: '@mayank01322310',
+    },
+    icons,
+    metadataBase: new URL("https://cobracase-dev.vercel.app/")
+  }
 }
