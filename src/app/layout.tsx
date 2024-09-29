4bb479147fc75d298d58ac3eb27a245b87bcc0bc
mayank2130 <mayankthakur1712@gmail.com>
2024-09-29 20:49:40 +0530

    website analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,39 @@
-import { Recursive } from 'next/font/google'
-import './globals.css'
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
-import { Toaster } from '@/components/ui/toaster'
-import Providers from '@/components/Providers'
-import { constructMetadata } from '@/lib/utils'
+import { Recursive } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import { Toaster } from "@/components/ui/toaster";
+import Providers from "@/components/Providers";
+import { constructMetadata } from "@/lib/utils";
+import Script from "next/script";
 
-const recursive = Recursive({ subsets: ['latin'] })
+const recursive = Recursive({ subsets: ["latin"] });
 
-export const metadata = constructMetadata()
+export const metadata = constructMetadata();
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body className={recursive.className}>
         <Navbar />
 
-        <main className='flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]'>
-          <div className='flex-1 flex flex-col h-full'>
+        <main className="flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]">
+          <div className="flex-1 flex flex-col h-full">
             <Providers>{children}</Providers>
           </div>
+          <Script
+            src="https://analytics-dashboard-ivory.vercel.app/analytics-script.js"
+            strategy="afterInteractive"
+          />
           <Footer />
         </main>
 
         <Toaster />
       </body>
     </html>
-  )
+  );
 }
